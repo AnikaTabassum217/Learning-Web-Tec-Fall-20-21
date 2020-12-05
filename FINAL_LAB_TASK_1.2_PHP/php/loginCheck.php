89cb8d013f3445fefc6622eaabd8419436dd87dd
AnikaTabassum217 <anika.tabassum579@gmail.com>
2020-12-05 09:19:56 +0600

    php

--- FINAL_LAB_TASK_1.2_PHP/php/loginCheck.php
+++ FINAL_LAB_TASK_1.2_PHP/php/loginCheck.php
@@ -1,25 +1,25 @@
 <?php
 	//session_start();
-	require_once('../models/usersService.php');
+	require_once('../models/EmployeesService.php');
 
 	if(isset($_REQUEST['submit'])){
-		$username = $_REQUEST['username'];
+		$employeename = $_REQUEST['employeename'];
 		$password = $_REQUEST['password'];
 
-		if(empty($username) || empty($password)){
+		if(empty($employeename) || empty($password)){
 			//echo "null submission";
 			header('location: ../view/login.php?msg=null');
 		}else{
 
-			$user = [
-				'username'=> $username,
+			$employee = [
+				'employeename'=> $employeename,
 				'password'=> $password
 			];
 
-			$status = validate($user);
+			$status = validate($employee);
 
 			if($status){
-				setcookie('flag', $username, time()+3600, '/');
+				setcookie('flag', $employeename, time()+3600, '/');
 				header('location: ../view/home.php');
 			}else{
 				header('location: ../view/login.php?msg=invalid');
