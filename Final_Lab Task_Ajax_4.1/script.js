903698f13fb0513d8c469c8a489603b63cba851e
AnikaTabassum217 <anika.tabassum579@gmail.com>
2020-12-14 09:23:33 +0600

    js

---
+++ Final_Lab Task_Ajax_4.1/script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+function ajax(){
+	let data= document.getElementById('name').value;
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.open('REQUEST', 'Login.php', true);
+	xhttp.open('REQUEST', 'Registration.php', true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	xhttp.onreadystatechange = function(){
+		
+		if (this.readyState == 4 && this.status == 200) {
+	    	document.getElementById('result').innerHTML = this.responseText;
+	    }
+	}
+	
+	xhttp.send('name='+data);
+}
